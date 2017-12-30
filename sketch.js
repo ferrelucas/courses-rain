1832b1333109239a13c952dedb2ed160662ef65d
Lucas Ferreira <ferreira.lkz@gmail.com>
2017-12-29 22:27:08 -0200

    iniciando - entendi como funciona

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var capture;
+
+function setup() {
+  createCanvas(1000, 240);
+  capture = createCapture(VIDEO);
+  capture.size(320, 240);
+  capture.hide();
+  
+  rSlider = createSlider(0, 100, 38);
+  rSlider.position(20, 20);
+
+  posSlider = createSlider(0, 100, 38);
+  posSlider.position(20, 50);
+}
+
+function draw() {
+  background(255);
+  push()
+  translate(capture.width * posSlider.value()/100, 0);
+  scale(-1,1);
+  image(capture, 0, 0, 320, 240);
+  pop()
+  filter('INVERT', rSlider.value()/100);
+  
+  push()
+  translate(capture.width, 0);
+  scale(-1,1);
+  image(capture, -320, 0, 320, 240);
+  pop()
+
+  filter('THRESHOLD', rSlider.value()/100);
+  push()
+  translate(capture.width, 0);
+  scale(-1,1);
+  image(capture, -640, 0, 320, 240);
+  pop()
+  
+  //filter('INVERT');
+  
+}
